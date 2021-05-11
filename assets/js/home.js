b628669ea429c5727f4fba6c413862d448248965
SergAlKoba <Serg.Al.Koba@gmail.com>
2021-05-11 10:06:15 +0300

    medical-center-slider fin

--- assets/js/home.js
+++ assets/js/home.js
@@ -6,11 +6,51 @@ $(function(){
     });
 
     $('.medical-center-slider').slick({
-        infinite: true,
-        slidesToShow: 3,
+        rows: 1,
+        infinite: false,
         slidesToScroll: 1,
+        slidesToShow: 2.28,
         nextArrow: '<button class="slick-arrow next"><i class="icon-slick-next"></i></button>',
         prevArrow: '<button class="slick-arrow prev"><i class="icon-slick-prev"></i></button>',
+        responsive: [
+            {
+                breakpoint: 1800,
+                settings: {
+                    slidesToShow: 2.28,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 1600,
+                settings: {
+                    slidesToShow: 2.6,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 1400,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToScroll: 1,
+                    slidesToShow: 2.6,
+                }
+            },
+            {
+              breakpoint: 1024,
+              settings: {
+                rows: 2,
+                infinite: true,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+              }
+            }
+        ]
     });
 
 });
